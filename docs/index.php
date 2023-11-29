b15ced706142098cf7d6551e6b6676a1f354b041
Barton Phillips <bartonphillips@gmail.com>
2023-11-29 12:09:51 -0500

    Added test items to docs. 	new file:   docs/index.php 	new file:   docs/mysitemap.json 	new file:   docs/site-class 	new file:   hitcounter.css

---
+++ docs/index.php
@@ -0,0 +1,24 @@
+<?php
+function callback($class) {
+  switch($class) {
+    case "SiteClass":
+      require(__DIR__ . "/site-class/includes/$class.class.php");
+      break;
+    default:
+      $class = preg_replace("~Simple~", "", $class);
+      require(__DIR__ . "/site-class/includes/database-engines/$class.class.php");
+      break;
+  }
+}
+
+if(spl_autoload_register("callback") === false) exit("Can't Autoload");
+
+require(__DIR__ . "/site-class/includes/database-engines/helper-functions.php");
+
+ErrorClass::setDevelopment(true);
+
+$_site = json_decode(stripComments(file_get_contents("./mysitemap.json")));
+
+$S = new SiteClass($_site);
+
+vardump("S", $S);
